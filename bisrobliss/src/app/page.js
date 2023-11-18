0b662f8445261603121453d56a4acfb245a2d6cc
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-11-18 20:49:49 +0300

    header

--- bisrobliss/src/app/page.js
+++ bisrobliss/src/app/page.js
@@ -3,7 +3,9 @@ import style from './page.module.scss';
 export default function Homepage() {
   return (
     <main className={style.main}>
-      
+      <section className={style.imgmain}></section>
+      <section className={style.blockmenu}></section>
+      <section className={style.services}></section>
     </main>
   )
 }
