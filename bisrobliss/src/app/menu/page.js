9221c8738f26492fce56804f3b896380bac93fe6
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-11-24 20:04:19 +0300

    footer

---
+++ bisrobliss/src/app/menu/page.js
@@ -0,0 +1,21 @@
+import style from '../menu/menu.module.scss';
+
+export default function Menu() {
+    const array = [{id:1, title: '', description: '', price: '', img: image1}, 
+]
+
+
+    return (
+        <div className={style.mainMenu}>
+            <h1>Our Menu</h1>
+            <p>
+                We consider all the drivers of change gives you the components
+                you need to change to create a truly happens.
+            </p>
+
+            <div className={style.container}>
+
+            </div>
+        </div>
+    )
+}
