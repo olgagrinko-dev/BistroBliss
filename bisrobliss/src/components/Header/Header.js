88000890baf6d168a2903ab54c443ab43591d888
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-11-28 17:53:07 +0300

    зубра

--- bisrobliss/src/components/Header/Header.js
+++ bisrobliss/src/components/Header/Header.js
@@ -4,42 +4,47 @@ import style from '../Header/header.module.scss';
 export default function Header() {
     return (
         <div className={style.header}>
-            <div className={style.info}>
+            <div className={style.colorinfo}>
 
-                <div className={style.contact}>
-                    <div className={style.phone}>
-                        <div className={style.imgphone}></div>
-                        <p>(414) 857 - 0107</p>
+                <div className={style.info}>
+                    <div className={style.contact}>
+                        <div className={style.phone}>
+                            <div className={style.imgphone}></div>
+                            <p>(414) 857 - 0107</p>
+                        </div>
+
+                        <div className={style.mail}>
+                            <div className={style.imgmail}></div>
+                            <p>yummy@bistrobliss</p>
+                        </div>
                     </div>
 
-                    <div className={style.mail}>
-                        <div className={style.imgmail}></div>
-                        <p>yummy@bistrobliss</p>
+                    <div className={style.images}>
+                        <div className={style.imgtwitter}></div>
+                        <div className={style.imgfacebook}></div>
+                        <div className={style.imgpinstagram}></div>
+                        <div className={style.imgcompan}></div>
                     </div>
-                </div>
-
-                <div className={style.images}>
-                    <div className={style.imgtwitter}></div>
-                    <div className={style.imgfacebook}></div>
-                    <div className={style.imgpinstagram}></div>
-                    <div className={style.imgcompan}></div>
                 </div>
             </div>
 
-            <div className={style.navig}>
-                <div className={style.logomain}>
-                    <div className={style.logo}></div>
-                    <h1>Bistro Bliss</h1>
-                </div>
+            <div className={style.colornavig}>
+                <div className={style.navig}>
+                    <div className={style.logomain}>
+                        <div className={style.logo}></div>
+                        <h1>Bistro Bliss</h1>
+                    </div>
 
-                <div className={style.elemnavig}>
-                    <p ><Link href='/'>home</Link></p>
-                    <p><Link href='/about'>about</Link></p>
-                    <p><Link href='/'>menu</Link></p>
-                </div>
+                    <div className={style.elemnavig}>
+                        <p ><Link href='/'>home</Link></p>
+                        <p><Link href='/about'>about</Link></p>
+                        <p><Link href='/'>menu</Link></p>
+                    </div>
 
-                <div className={style.btnhed}>Book A Table</div>
+                    <div className={style.btnhed}>Book A Table</div>
+                </div>
             </div>
+
         </div>
     )
 }
