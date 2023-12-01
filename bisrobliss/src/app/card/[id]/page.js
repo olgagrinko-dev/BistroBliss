22f67607384f0666ef0a4717f658c06530f005f7
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-12-01 21:21:41 +0300

    card

---
+++ bisrobliss/src/app/card/[id]/page.js
@@ -0,0 +1,33 @@
+'use client'
+
+import style from '../[id]/id.module.scss';
+import Image from 'next/image';
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import storage from '../../../storage/storage';
+
+export default function Id() {
+    const { id } = useParams();
+    const [found, setFound] = useState({});
+
+    useEffect(() => {
+        const result = storage.filter(el => el.id == id);
+        setFound(result[0]);
+        console.log(result);
+    }, []);
+
+    return (
+        <div className={style.colorCard}>
+            <div className={style.mainCards}>
+                <div className={style.imgCard}>
+                    <Image className={style.img} src={found.img}></Image>
+                </div>
+                <div className={style.textCard}>
+                    <h1>{found?.title}</h1>
+                    <h2>{found?.description}</h2>
+                    <p>{found?.price}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ bisrobliss/src/storage/storage.js
@@ -0,0 +1,20 @@
+import img1 from '../app/menu/assets/img1.png';
+import img2 from '../app/menu/assets/img2.png';
+import img3 from '../app/menu/assets/img3.png';
+import img4 from '../app/menu/assets/img4.png';
+import img5 from '../app/menu/assets/img5.png';
+import img6 from '../app/menu/assets/img6.png';
+import img7 from '../app/menu/assets/img7.png';
+import img8 from '../app/menu/assets/img8.png';
+
+const array = [{ id: 1, title: 'Fried Eggs', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 9.99', img: img1 },
+{ id: 2, title: 'Hawaiian Pizza', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 15.99', img: img2 },
+{ id: 3, title: 'Martinez Cocktail', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 7.25', img: img3 },
+{ id: 4, title: 'Butterscotch Cake', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 20.99', img: img4 },
+{ id: 5, title: 'Mint Lemonade', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 5.89', img: img5 },
+{ id: 6, title: 'Chocolate Icecream', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 18.05', img: img6 },
+{ id: 7, title: 'Cheese Burger', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 12.55', img: img7 },
+{ id: 8, title: 'Classic Waffles', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 12.99', img: img8 }
+];
+
+export default array;
