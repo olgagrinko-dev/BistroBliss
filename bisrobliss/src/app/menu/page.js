86c08173bfc0b65446b17cb2cac14678e9aa9c5a
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-12-04 18:32:01 +0300

    array.map

--- bisrobliss/src/app/menu/page.js
+++ bisrobliss/src/app/menu/page.js
@@ -2,30 +2,10 @@
 
 import style from '../menu/menu.module.scss';
 import Image from 'next/image';
-import img1 from './assets/img1.png';
-import img2 from './assets/img2.png';
-import img3 from './assets/img3.png';
-import img4 from './assets/img4.png';
-import img5 from './assets/img5.png';
-import img6 from './assets/img6.png';
-import img7 from './assets/img7.png';
-import img8 from './assets/img8.png';
 import Link from 'next/link';
+import array from '../../storage/storage'
 
 export default function Menu() {
-    const array = [{ id: 1, title: 'Fried Eggs', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 9.99', img: img1 },
-    { id: 2, title: 'Hawaiian Pizza', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 15.99', img: img2 },
-    { id: 3, title: 'Martinez Cocktail', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 7.25', img: img3 },
-    { id: 4, title: 'Butterscotch Cake', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 20.99', img: img4 },
-    { id: 5, title: 'Mint Lemonade', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 5.89', img: img5 },
-    { id: 6, title: 'Chocolate Icecream', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 18.05', img: img6 },
-    { id: 7, title: 'Cheese Burger', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 12.55', img: img7 },
-    { id: 8, title: 'Classic Waffles', description: 'Made with eggs, lettuce, salt, oil and other ingredients.', price: '$ 12.99', img: img8 }
-    ]
-
-    function getResult(event) {
-        console.log(event.currentTarget);
-    }
 
     return (
         <div className={style.mainMenu}>
@@ -40,15 +20,15 @@ export default function Menu() {
             <div className={style.container}>
 
                 {array.map((elem) => <Link href={`/menu/${elem.id}`}>
-                        <div className={style.item} onClick={getResult} >
-                            <div><Image src={elem.img} /></div>
-                            <div className={style.containerText}>
-                                <p className={style.colorelem}>{elem.price}</p>
-                                <h2>{elem.title}</h2>
-                                <p>{elem.description}</p>
-                            </div>
+                    <div className={style.item} >
+                        <div><Image src={elem.img} /></div>
+                        <div className={style.containerText}>
+                            <p className={style.colorelem}>{elem.price}</p>
+                            <h2>{elem.title}</h2>
+                            <p>{elem.description}</p>
                         </div>
-                    </Link>
+                    </div>
+                </Link>
                 )}
 
             </div>
--- bisrobliss/src/app/page.js
+++ bisrobliss/src/app/page.js
@@ -1,6 +1,32 @@
 import style from './page.module.scss';
+import Image from 'next/image';
+import img1app from '../../public/assets/HomePage/tea.svg'
+import img2app from '../../public/assets/HomePage/icon2.svg'
+import img3app from '../../public/assets/HomePage/icon3.svg'
+import img4app from '../../public/assets/HomePage/icon4.svg'
+
+import img5app from '../../public/assets/HomePage/kebab.svg'
+import img6app from '../../public/assets/HomePage/birthday.svg'
+import img7app from '../../public/assets/HomePage/happy.svg'
+import img8app from '../../public/assets/HomePage/group.svg'
+
+
 
 export default function Homepage() {
+  const array1 = [
+    { img1: img1app, title: 'Breakfast', description: 'In the new era of technology we look in the future with certainty and pride for our life.', button: 'Explore Menu' },
+    { img1: img2app, title: 'Main Dishes', description: 'In the new era of technology we look in the future with certainty and pride for our life.', button: 'Explore Menu' },
+    { img1: img3app, title: 'Drinks', description: 'In the new era of technology we look in the future with certainty and pride for our life.', button: 'Explore Menu' },
+    { img1: img4app, title: 'Desserts', description: 'In the new era of technology we look in the future with certainty and pride for our life.', button: 'Explore Menu' }
+  ]
+
+  const array2 = [
+    { img2: img5app, event: 'Caterings', description: 'In the new era of technology we look in the future with certainty for life.' },
+    { img2: img6app, event: 'Birthdays', description: 'In the new era of technology we look in the future with certainty for life.' },
+    { img2: img7app, event: 'Weddings', description: 'In the new era of technology we look in the future with certainty for life.' },
+    { img2: img8app, event: 'Events', description: 'In the new era of technology we look in the future with certainty for life.' }
+  ]
+
   return (
 
     <div className={style.colormain}>
@@ -9,9 +35,11 @@ export default function Homepage() {
         <section className={style.title}>
           <div className={style.text}>
             <h1>Best food for
-              your taste</h1>
+              your taste
+            </h1>
             <p>Discover delectable cuisine and unforgettable moments
-              in our welcoming, culinary haven.</p>
+              in our welcoming, culinary haven.
+            </p>
             <div className={style.btns}>
               <button className={style.btnbron}>Book A Table</button>
               <button className={style.btnmenu}>Explore Menu</button>
@@ -21,103 +49,34 @@ export default function Homepage() {
 
         <div className={style.colorblockmenu}>
           <section className={style.blockmenu}>
+
             <h1>Browse Our Menu</h1>
+
             <div className={style.foodselection}>
-
-              <div className={style.breakfast}>
-                <div className={style.imgbreakfast}></div>
-                <h2>Breakfast</h2>
-                <p>
-                  In the new era of technology we
-                  look in the future with certainty
-                  and pride for our life.
-                </p>
-                <div className={style.btnblock}>Explore Menu</div>
-              </div>
-
-              <div className={style.maindishes}>
-                <div className={style.imgmaindishes}></div>
-                <h2>Main Dishes</h2>
-                <p>
-                  In the new era of technology we
-                  look in the future with certainty
-                  and pride for our life.
-                </p>
-                <div className={style.btnblock}>Explore Menu</div>
-              </div>
-
-              <div className={style.drinks}>
-                <div className={style.imgdrinks}></div>
-                <h2>Drinks</h2>
-                <p>
-                  In the new era of technology we
-                  look in the future with certainty
-                  and pride for our life.
-                </p>
-                <div className={style.btnblock}>Explore Menu</div>
-              </div>
-
-              <div className={style.desserts}>
-                <div className={style.imgdesserts}></div>
-                <h2>Desserts</h2>
-                <p>
-                  In the new era of technology we
-                  look in the future with certainty
-                  and pride for our life.
-                </p>
-                <div className={style.btnblock}>Explore Menu</div>
-              </div>
+              {array1.map((elem) =>
+                <div className={style.itemMain}>
+                  <div><Image src={elem.img1} /></div>
+                  <h2>{elem.title}</h2>
+                  <p>{elem.description}</p>
+                  <div className={style.btnblock}>Explore Menu</div>
+                </div>
+              )}
             </div>
           </section>
         </div>
 
         <section className={style.services}>
+
           <h1>We also offer unique services for your events</h1>
 
           <div className={style.events}>
-            <div className={style.caterings}>
-              <div className={style.imgcaterings}></div>
-              <div className={style.text}>
-                <h2>Caterings</h2>
-                <p>
-                  In the new era of technology we look
-                  in the future with certainty for life.
-                </p>
+            {array2.map((elem) =>
+              <div className={style.itemEvents}>
+                <div><Image src={elem.img2} /></div>
+                <h2>{elem.event}</h2>
+                <p>{elem.description}</p>
               </div>
-            </div>
-
-            <div className={style.birthdays}>
-              <div className={style.imgbirthdays}></div>
-              <div className={style.text}>
-                <h2>Birthdays</h2>
-                <p>
-                  In the new era of technology we look
-                  in the future with certainty for life.
-                </p>
-              </div>
-            </div>
-
-            <div className={style.weddings}>
-              <div className={style.imgweddings}></div>
-              <div className={style.text}>
-                <h2>Weddings</h2>
-                <p>
-                  In the new era of technology we look
-                  in the future with certainty for life.
-                </p>
-              </div>
-            </div>
-
-            <div className={style.event}>
-              <div className={style.imgevents}></div>
-              <div className={style.text}>
-                <h2>Events</h2>
-                <p>
-                  In the new era of technology we look
-                  in the future with certainty for life.
-                </p>
-              </div>
-            </div>
+            )}
           </div>
         </section>
       </main>
